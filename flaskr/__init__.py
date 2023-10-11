b30bb3d58e58132e8cda5dabc4f76b7e3ab2ddf7
tiennguyen13 <tiennguyen@teqnological.asia>
2023-10-11 11:43:17 +0700

    Initialize the Database File

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -36,4 +36,8 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
+    # Initialize the Database File
+    from . import db
+    db.init_app(app)
+
     return app
--- makefile
+++ makefile
@@ -2,4 +2,5 @@ install:
 	pip install -r requirements.txt
 
 run:
+	flask --app flaskr init-db
 	flask --app flaskr run --debug
